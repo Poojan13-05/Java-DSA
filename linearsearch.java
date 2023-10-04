8638f03056d0cc85edc1554889cfdabb772d9f8c
Poojan Prajapati <panthprajapati1305@gmail.com>
2023-10-04 21:21:28 +0530

    linear search for string

--- linearsearch.java
+++ linearsearch.java
@@ -11,8 +11,8 @@ public class linearsearch {
             return -1;
         }
         for (int i = 0; i < arr.length ; i++) {
-            int element=arr[i];
-            if (element==target){
+
+            if (target==arr[i]){
                 return i;
             }
 
